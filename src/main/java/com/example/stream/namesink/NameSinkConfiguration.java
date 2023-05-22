d229977e8941b2826b79b6e1ff2a210b652dbc51
Mark Pollack <mpollack@vmware.com>
2023-05-22 11:47:33 -0400

    initial commit

---
+++ src/main/java/com/example/stream/namesink/NameSinkConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.stream.namesink;
+
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NameSinkConfiguration {
+
+	@Bean
+	public Consumer<Person> nameSink() {
+		return person -> {
+			System.out.println(person.name());
+			System.out.println(person.processedTimestamp());
+		};
+	}
+}
